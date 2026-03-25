438b7932c4852f38703656a9f2be11cbe9191909
Mateusz Byczek <kontakt@mbyczek.eu>
2026-03-25 23:36:31 +0100

    remove unused commands from header file
    - backup of home assistant yaml sensors config

--- include/inverter_commands.h
+++ include/inverter_commands.h
@@ -10,12 +10,11 @@ const int MAX_RETRIES = 3;
 static const byte cmd_QPIGS[] = {0x51, 0x50, 0x49, 0x47, 0x53, 0xB7, 0xA9, 0x0D}; 
 static const byte cmd_QMOD[]  = {0x51, 0x4D, 0x4F, 0x44, 0x49, 0xC1, 0x0D};
 static const byte cmd_QPIRI[] = {0x51, 0x50, 0x49, 0x52, 0x49, 0xF8, 0x54, 0x0D};
-static const byte cmd_QID[]   = {0x51, 0x49, 0x44, 0xD6, 0xEA, 0x0D};
 
 // Tabela wskaźników do komend i ich rozmiarów
-static const byte* ALL_COMMANDS[] = {cmd_QPIGS, cmd_QMOD, cmd_QPIRI, cmd_QID};
-static const int CMD_SIZES[] = {8, 7, 8, 6};
-static const char* CMD_NAMES[] = {"QPIGS", "QMOD", "QPIRI", "QID"};
-static const int CMD_COUNT = 4;
+static const byte* ALL_COMMANDS[] = {cmd_QPIGS, cmd_QMOD, cmd_QPIRI};
+static const int CMD_SIZES[] = {8, 7, 8};
+static const char* CMD_NAMES[] = {"QPIGS", "QMOD", "QPIRI"};
+static const int CMD_COUNT = 3;
 
 #endif // INVERTER_COMMANDS_H
